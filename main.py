d8764fbcda7893cf6e9fdbba33d3f0753887e08f
Stan <stankv@mail.ru>
2024-12-17 15:56:38 +0300

    crud methods added

---
+++ main.py
@@ -0,0 +1,116 @@
+import uvicorn
+from fastapi import FastAPI, Query, Body
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+
+# --------------------------------------------------------------------------------------
+# решение проблемы нкорректной работы документации
+# https://fastapi.tiangolo.com/how-to/custom-docs-ui-assets/#disable-the-automatic-docs
+app = FastAPI(docs_url=None, redoc_url=None)
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
+    )
+#--------------------------------------------------------------------------------------
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+]
+
+@app.get("/hotels")
+def get_hotels(
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def update_hotel(
+        id: int = Body(embed=True),
+        title: str = Body(embed=True),
+        name: str = Body(embed=True),
+):
+    global hotels
+    hotels = [
+        hotel if hotel["id"] != id else {
+            "id": id,
+            "title": title,
+            "name": name
+        }
+        for hotel in hotels
+    ]
+    return {"status": "OK"}
+
+
+@app.patch("/hotels/{hotel_id}")
+def patch_hotel(
+        id: int = Body(embed=True),
+        title: str | None = Body(None, embed=True, description="Название отеля"),
+        name: str | None = Body(None, embed=True, description="транскрипция названия отеля"),
+):
+    global hotels
+    if title is None and name is None:
+        return {"status": "Data not changed"}
+    hotels = [
+        hotel if hotel["id"] != id else {
+            **hotel,
+            **{
+                "title": title if title else hotel["title"],
+                "name": name if name else hotel["name"],
+            }
+        }
+        for hotel in hotels
+    ]
+    return {"status": "OK"}
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
