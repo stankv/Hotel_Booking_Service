61328ba802d12b072dfab743dd2c54d65e53fc6d
Stan <stankv@mail.ru>
2025-01-13 14:42:25 +0300

    added Base class for working with ORM

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
@@ -6,4 +7,5 @@ engine = create_async_engine(settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-session = async_session_maker()
+class Base(DeclarativeBase):
+    pass
