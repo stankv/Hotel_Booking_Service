d7d479d6d8503f82bd07c9c4ba80878270870e58
Stan <stankv@mail.ru>
2025-01-15 16:39:41 +0300

    migration rooms done, error fixed

--- src/models/rooms.py
+++ src/models/rooms.py
@@ -8,7 +8,7 @@ class RoomsOrm(Base):
     __tablename__ = "rooms"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    hotel_id: Mapped[int] = mapped_column(ForeingKey("hotels.id"))
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
     title: Mapped[str]
     description: Mapped[str | None]
     price: Mapped[int]
