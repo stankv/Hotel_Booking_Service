d82ba2b0f71783e0e970a98c322fbefdca3b85e4
Stan <stankv@mail.ru>
2025-01-20 16:41:30 +0300

    GET: pagination and filtration added

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 
 class PaginationParams(BaseModel):
-    page: Annotated[int | None, Query(None, gt=0, description="Номер страницы")]
+    page: Annotated[int | None, Query(1, gt=0, description="Номер страницы")]
     per_page: Annotated[int | None, Query(None, gt=0, lt=30, description="Количество элементов на странице")]
 
 
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -19,8 +19,18 @@ async def get_hotels(
         id: int | None = Query(None, description="ID отеля"),
         title: str | None = Query(None, description="Название отеля"),
 ):
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
         query = select(HotelsOrm)
+        if id:
+            query = query.filter_by(id=id)
+        if title:
+            query = query.filter_by(title=title)
+        query = (
+            query
+            .limit(per_page)
+            .offset((pagination.page - 1) * per_page)
+        )
         result = await session.execute(query)
         hotels = result.scalars().all()
         return hotels
