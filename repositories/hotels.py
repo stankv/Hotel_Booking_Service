ff0396f51e702821804706ce5f47cf7927a088c4
Stan <stankv@mail.ru>
2025-01-27 16:13:29 +0300

    get_all() added to HotelsRepository and use it in GET api/hotels

--- repositories/hotels.py
+++ repositories/hotels.py
@@ -1,5 +1,27 @@
+from sqlalchemy import select, func
+
 from repositories.base import BaseRepository
 from src.models.hotels import HotelsOrm
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
+
+    async def get_all(
+            self,
+            title,
+            location,
+            limit,
+            offset,
+    ):
+        query = select(HotelsOrm)
+        if title:
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+        if location:
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+        query = (
+            query
+            .limit(limit)
+            .offset(offset)
+        )
+        result = await self.session.execute(query)
+        return result.scalars().all()
