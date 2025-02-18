f8294e906e5416340af11be6315dbd37c67bc5c4
Stan <stankv@mail.ru>
2025-02-18 15:33:07 +0300

    generating JWT-token added, endpoint login_user created

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from datetime import datetime, timezone, timedelta
+
+from fastapi import APIRouter, HTTPException
 from passlib.context import CryptContext
+import jwt
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -8,6 +11,32 @@ from src.schemas.users import UserRequestAdd, UserAdd
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@router.post("/login",
+             summary="Аутентификация",
+             description="<h1>Аутентификация (вход) пользователя</h1>"
+             )
+async def login_user(
+        data: UserRequestAdd,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+        access_token = create_access_token({"user_id": user.id})
+        return {"access_token": access_token}
+
 
 @router.post("/register",
              summary="Регистрация",
