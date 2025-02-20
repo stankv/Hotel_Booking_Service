6e652a77d19730a0c93485a1a4bc04dcee56b7b9
Stan <stankv@mail.ru>
2025-02-20 14:51:18 +0300

    schema added

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,5 +1,8 @@
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
+from src.schemas.rooms import Room
+
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
+    schema = Room
