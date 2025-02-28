bf094d9d8766a0352502e2df4010bea308d772bb
Stan <stankv@mail.ru>
2025-02-28 16:56:27 +0300

    get_filtered() and get_filtered_by_time() changed

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,18 +1,19 @@
 from pydantic import BaseModel
 from sqlalchemy import select, insert, update, delete
 
-from src.schemas.hotels import Hotel
 
-
-class BaseRepository():
+class BaseRepository:
     model = None
     schema: BaseModel = None
 
     def __init__(self, session):
         self.session = session
 
-    async def get_filtered(self, **filter_by):
-        query = select(self.model).filter_by(**filter_by)
+    async def get_filtered(self, *filter, **filter_by):
+        query = (
+            select(self.model)
+            .filter(*filter)
+            .filter_by(**filter_by))
         result = await self.session.execute(query)
         return [self.schema.model_validate(model) for model in result.scalars().all()]
 
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -19,11 +19,6 @@ class RoomsRepository(BaseRepository):
             date_from: date,
             date_to: date
     ):
-        """
-        !select room_id, count(*) as rooms_booked from bookings
-        where date_from <= '2024-11-07' and date_to >= '2024-07-01'
-        group by room_id
-        """
         rooms_count = (
             select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
             .select_from(BookingsOrm)
@@ -32,16 +27,9 @@ class RoomsRepository(BaseRepository):
                 BookingsOrm.date_to >= date_from,
             )
             .group_by(BookingsOrm.room_id)
-            .cte(name="rooms_count")    # это WITH rooms_count AS
+            .cte(name="rooms_count")
         )
 
-        """
-        rooms_left_table as (
-            select rooms.id as room_id, quantity - coalesce(rooms_booked, 0) as rooms_left
-            from rooms
-            left join rooms_count on rooms.id = rooms_count.room_id
-        )
-        """
         rooms_left_table = (
             select(
                 RoomsOrm.id.label("room_id"),
@@ -49,17 +37,25 @@ class RoomsRepository(BaseRepository):
             )
             .select_from(RoomsOrm)
             .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
-            .cte(name="rooms_left_table")    # тоже относится к WITH
+            .cte(name="rooms_left_table")
         )
 
-        """
-        !select * from rooms_left_table
-        where rooms_left > 0
-        """
-        query = (
-            select(rooms_left_table)
+        rooms_ids_for_hotel = (
+            select(RoomsOrm.id)
+            .select_from(RoomsOrm)
+            .filter_by(hotel_id=hotel_id)
+            .subquery(name="rooms_ids_for_hotel")
+        )
+
+        rooms_ids_to_get = (
+            select(rooms_left_table.c.room_id)
             .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0)
+            .filter(
+                rooms_left_table.c.rooms_left > 0,
+            rooms_left_table.c.room_id.in_(rooms_ids_for_hotel),
+            )
         )
 
-        print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
+        #print(rooms_ids_to_get.compile(bind=engine, compile_kwargs={"literal_binds": True}))
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
