d6ac3719c3d9fc73770d5bd2c598230f2a055ad1
Stan <stankv@mail.ru>
2025-03-06 17:58:38 +0300

    PUT & PATCH api/rooms: allow edit facilities

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -50,6 +50,7 @@ async def create_room(hotel_id: int, db: DBDep, room_data: RoomAddRequest = Body
 async def update_room(hotel_id: int, room_id: int, room_data: RoomAddRequest, db: DBDep):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     await db.rooms.edit(_room_data, id=room_id)
+    await db.rooms_facilities.set_room_facilities(room_id, facilities_ids=room_data.facilities_ids)
     await db.commit()
     return {"status": "OK"}
 
@@ -59,8 +60,11 @@ async def update_room(hotel_id: int, room_id: int, room_data: RoomAddRequest, db
            description="<h1>Можно изменить любое поле</h1>"
            )
 async def partially_update_room(hotel_id: int, room_id: int, room_data: RoomPatchRequest, db: DBDep):
-    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
+    _room_data_dict = room_data.model_dump(exclude_unset=True)
+    _room_data = RoomPatch(hotel_id=hotel_id, **_room_data_dict)
     await db.rooms.edit(_room_data, exclude_unset=True, id=room_id, hotel_id=hotel_id)
+    if "facilities_ids" in _room_data_dict:
+        await db.rooms_facilities.set_room_facilities(room_id, facilities_ids=_room_data_dict["facilities_ids"])
     await db.commit()
     return {"status": "OK"}
 
--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -6,7 +6,7 @@ class RoomAddRequest(BaseModel):
     description: str | None = None  # при вариативности надо задавать значение
     price: int
     quantity: int
-    facilities_ids: list[int] | None = None
+    facilities_ids: list[int] = []
 
 
 class RoomAdd(BaseModel):
@@ -27,6 +27,7 @@ class RoomPatchRequest(BaseModel):
     description: str | None = None
     price: int | None = None
     quantity: int | None = None
+    facilities_ids: list[int] = []
 
 
 class RoomPatch(BaseModel):
