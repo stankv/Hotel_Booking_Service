874cd15bbe6c1a9a3dfbcad4463a3af5c66e4c99
Stan <stankv@mail.ru>
2025-03-19 14:09:51 +0300

    add /images router, add POST /images for uploading image, add celery task resize_image()

--- src/main.py
+++ src/main.py
@@ -13,15 +13,15 @@ from fastapi_cache.backends.redis import RedisBackend
 import sys
 from pathlib import Path
 
-from init import redis_manager    # from src.init import redis_manager
-
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.init import redis_manager    # from src.init import redis_manager
 from src.api.auth import router as router_auth
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
+from src.api.images import router as router_images
 
 
 @asynccontextmanager
@@ -43,6 +43,7 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_facilities)
 app.include_router(router_bookings)
+app.include_router(router_images)
 
 
 @app.get("/docs", include_in_schema=False)
