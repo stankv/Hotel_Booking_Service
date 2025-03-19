c8a8a81117fdd1997ddb0c6d5e84934293f547c0
Stan <stankv@mail.ru>
2025-03-19 14:04:58 +0300

    error in paths fixed

--- src/init.py
+++ src/init.py
@@ -1,5 +1,5 @@
-from connectors.redis_connector import RedisManager  # from src.connectors.redis_connector import RedisManager
-from config import settings  # from src.config import settings
+from src.connectors.redis_connector import RedisManager
+from src.config import settings
 
 redis_manager = RedisManager(
     host=settings.REDIS_HOST,
