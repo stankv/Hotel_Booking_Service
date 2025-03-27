d9db4768d8896083fa2113ef82f112643500f57f
Stan <stankv@mail.ru>
2025-03-27 15:28:16 +0300

    fixture for creating test db added

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -7,4 +7,5 @@ async def test_add_hotel():
     hotel_data = HotelAdd(title="Hotel 5 stars", location="Сочи")  # создаем схему
     async with DBManager(session_factory=async_session_maker) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
+        await db.commit()
         print(f"{new_hotel_data=}")
