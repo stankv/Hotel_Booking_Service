6eef9ae614c86093ef07740449e4cdc5a0735b23
Stan <stankv@mail.ru>
2025-04-15 13:35:50 +0300

    mock for fastapi_cache added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import json
+from unittest import mock
+
+mock.patch("fastapi_cache.decorator.cache", lambda *args, **kwargs: lambda f: f).start()
 
 import pytest
 from httpx import AsyncClient, ASGITransport
