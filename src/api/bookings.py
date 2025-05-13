9ca66aeedbe38506ac97b94f8b5d7519e606fc65
Stan <stankv@mail.ru>
2025-05-13 14:17:11 +0300

    create add_booking() to check whether room can be booked

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -34,12 +34,13 @@ async def add_booking(
     if user_id is None:
         return {"status": "ERROR", "message": "Пользователь не авторизован"}
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    hotel = await db.hotels.get_one_or_none(id=room.hotel_id)
     room_price: int = room.price
     _booking_data = BookingAdd(
         user_id=user_id,
         price=room_price,
         **booking_data.model_dump(),
     )
-    booking = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
     await db.commit()
     return {"status": "OK", "data":booking}
--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -5,6 +5,8 @@ from sqlalchemy import select
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.mappers import BookingDataMapper
+from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.bookings import BookingAdd
 
 
 class BookingsRepository(BaseRepository):
@@ -18,3 +20,18 @@ class BookingsRepository(BaseRepository):
         )
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
+
+    async def add_booking(self, data: BookingAdd, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=data.date_from,
+            date_to=data.date_to,
+            hotel_id=hotel_id,
+        )
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        rooms_ids_to_book: list[int] = rooms_ids_to_book_res.scalars().all()
+
+        if data.room_id in rooms_ids_to_book:    # если текущий номер в тех что можно бронировать, то бронируем
+            new_booking = await self.add(data)
+            return new_booking
+        else:
+            raise Exception
