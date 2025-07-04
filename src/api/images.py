be6a48fe794238b68d6ce4022ffd2231e0408c89
Stan <stankv@mail.ru>
2025-07-04 19:43:13 +0300

    description for docs (UI Swagger) added

--- src/api/images.py
+++ src/api/images.py
@@ -7,6 +7,10 @@ from src.tasks.tasks import resize_image
 router = APIRouter(prefix="/images", tags=["Изображения отелей"])
 
 
-@router.post("")
+@router.post(
+    "",
+    summary="Добавление изображения",
+    description="<h1>Добавление фото отеля</h1>",
+)
 def upload_image(file: UploadFile, background_tasks: BackgroundTasks):
     ImagesService().upload_image(file, background_tasks)
