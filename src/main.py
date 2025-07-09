928bb045387165c81ab119e03e9ea83500a8dd6d
Stan <stankv@mail.ru>
2025-07-09 19:01:29 +0300

    Dockerfile and .dockerignore created, change host to 0.0.0.0

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ import uvicorn
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 from src.init import redis_manager  # from src.init import redis_manager
 from src.api.auth import router as router_auth
@@ -78,4 +78,4 @@ async def redoc_html():
 # --------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
